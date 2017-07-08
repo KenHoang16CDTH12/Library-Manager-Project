30f40c3dbce061cb791cf245e52d60a1808001c5
KenHoang16CDTH12 <angelkittybihoang@gmail.com>
2017-07-08 12:25:45 +0700

    Create package util

---
+++ src/kenhoang/util/Config.java
@@ -0,0 +1,16 @@
+package kenhoang.util;
+
+/**
+ * Created by sample on 7/7/17.
+ */
+public class Config {
+    //Login
+    public static final String NOTIFICATIONS_LOGIN = "Thông báo đăng nhập";
+    public static final String LOGIN_FAILED = "Đăng nhập thất bại. Vui lòng nhập lại thông tin";
+    public static final String LOGIN_SUCCESS = "Đăng nhập thành công!";
+    //Exit
+    public static final String NOTIFICATION_EXIT = "Thoát";
+    public static final String HEADER_EXIT = "Xác nhận thoát phần mềm.";
+    public static final String CONTENT_EXIT = "Bạn chắc chắn muốn thoát chương trình ?";
+
+}
